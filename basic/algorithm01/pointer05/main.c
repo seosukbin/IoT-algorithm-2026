ff84cc82bd2e56ea63710d6e64f6a0e4991fa5c4
tanggu293 <dudwn1387@gmail.com>
2026-02-23 09:05:34 +0900

    x

--- basic/algorithm01/pointer05/main.c
+++ basic/algorithm01/pointer05/main.c
@@ -8,6 +8,10 @@ typedef struct
 
 int main(void)
 {
-	Person
+	Person p1 = { .name = "Hugo",.age = 50 };
+
+	Person* ptr = &p1;
+	printf("이름: %s\n", ptr->name); //*(ptr).name과 같다.
+	printf("나이: %d\n", ptr->age);
 	return 0;
 }
